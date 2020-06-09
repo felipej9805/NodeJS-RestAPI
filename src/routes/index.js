106d7334e8b8c5a4bc56cf4debd7801c7b8089bb
Felipe <felipejm9805@gmail.com>
2020-06-09 09:07:10 -0700

    Create the basic server with NodeJs

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const router = Router();
+
+
+
+router.get('/', (req, res) => {
+    const data = {
+        "name": "Felipe",
+        "age": "22"
+    }
+    res.json(data);
+})
+
+module.exports = router;
---
+++ src/routes/movies.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const router = Router();
+
+
+const movies = require('../sample.json');
+
+
+module.exports = router;
